49d727217a8a1e8219537f7ea5b92ff55556459e
silvarc141 <silvarc141@users.noreply.github.com>
2025-08-06 13:37:06 +0200

    standardize exported module

--- flake.nix
+++ flake.nix
@@ -1,5 +1,10 @@
 {
   description = "firefox-modal, a small nix-configured Vimium-like privileged firefox modification for home-manager";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-  outputs = {...}: { homeManagerModules.firefoxModal = import ./.; };
+  outputs = {...}: { 
+    homeManagerModules = rec {
+      firefoxModal = import ./.; 
+      default = firefoxModal;
+    };
+  };
 }
