a46e12972881a914471b7b812b7fafefba6f8f6d
silvarc141 <silvarc141@users.noreply.github.com>
2026-04-10 22:29:24 +0200

    try if sandbox is the issue after update

--- home-manager.nix
+++ home-manager.nix
@@ -3,17 +3,31 @@
   config,
   pkgs,
   ...
-}: let
+}:
+let
   inherit (lib) mkIf;
-in {
-  options = import ./options.nix {inherit lib;};
+in
+{
+  options = import ./options.nix { inherit lib; };
 
   config = mkIf (config.programs.firefox.enable && config.programs.firefox.modal.enable) {
-    programs.firefox.package = let
-      cfg = config.programs.firefox.modal;
-      generateFirefoxModalPrefs = import ./lib.nix {inherit lib;};
-      final = generateFirefoxModalPrefs cfg;
-    in
-      pkgs.firefox.override {extraPrefs = final;};
+    programs.firefox.package =
+      let
+        cfg = config.programs.firefox.modal;
+        generateFirefoxModalPrefs = import ./lib.nix { inherit lib; };
+        modalSource = generateFirefoxModalPrefs cfg;
+        wrappedPackage = pkgs.firefox.override { extraPrefs = modalSource; };
+      in
+      pkgs.runCommand "${wrappedPackage.name}-no-sandbox"
+        {
+          nativeBuildInputs = [ pkgs.bash ];
+        }
+        ''
+          mkdir -p $out
+          ln -s ${wrappedPackage}/* $out/
+          rm $out/lib/firefox/defaults/pref/autoconfig.js
+          cat ${wrappedPackage}/lib/firefox/defaults/pref/autoconfig.js > $out/lib/firefox/defaults/pref/autoconfig.js
+          echo 'pref("general.config.sandbox_enabled", false);' >> $out/lib/firefox/defaults/pref/autoconfig.js
+        '';
   };
 }
