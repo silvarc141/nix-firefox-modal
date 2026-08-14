db77347140167fd6db567d8ae8a9b74325c10727
silvarc141 <silvarc141@users.noreply.github.com>
2026-08-15 01:10:07 +0200

    wip update

--- flake.nix
+++ flake.nix
@@ -1,23 +1,34 @@
 {
   description = "firefox-modal, a small nix-configured Vimium-like privileged firefox modification for home-manager";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-  outputs = {nixpkgs, ...}: {
-    homeManagerModules = rec {
-      firefoxModal = import ./home-manager.nix;
-      default = firefoxModal;
-    };
-    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
-    packages.x86_64-linux.firefox-modal-prefs = let
-      lib = nixpkgs.lib;
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-
-      generateFirefoxModalPrefs = import ./lib.nix {inherit lib;};
-      moduleOptions = (import ./options.nix {inherit lib;}).programs.firefox.modal;
-      defaultCfg = lib.mapAttrs (name: option: option.default) moduleOptions;
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+  outputs =
+    { nixpkgs, ... }:
+    let
+      inherit (nixpkgs.lib) genAttrs;
+      allSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
     in
-      pkgs.writeTextFile {
-        name = "firefox-modal-prefs.js";
-        text = generateFirefoxModalPrefs defaultCfg;
+    {
+      homeManagerModules = rec {
+        firefoxModal = import ./home-manager.nix;
+        default = firefoxModal;
       };
-  };
+      formatter = genAttrs allSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
+      packages = genAttrs allSystems (system: {
+        firefox-modal-prefs =
+          let
+            lib = nixpkgs.lib;
+            pkgs = nixpkgs.legacyPackages.${system};
+            generateFirefoxModalPrefs = import ./lib.nix { inherit lib; };
+            moduleOptions = (import ./options.nix { inherit lib; }).programs.firefox.modal;
+            defaultCfg = lib.mapAttrs (name: option: option.default) moduleOptions;
+          in
+          pkgs.writeTextFile {
+            name = "firefox-modal-prefs.js";
+            text = generateFirefoxModalPrefs defaultCfg;
+          };
+      });
+    };
 }
